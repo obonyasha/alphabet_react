af023e78ae80ed6b5cd1bd58de7ab45584378e41
Tatiana Stepanova <obonyasha@gmail.com>
2023-09-19 18:10:48 +0500

    create Main

---
+++ src/components/Challenge/index.jsx
@@ -0,0 +1,7 @@
+const Challenge = () => {
+    return(
+        <h2>Сервис по изучнию алфавита</h2>
+    )
+}
+
+export default Challenge;
---
+++ src/components/Main/index.jsx
@@ -0,0 +1,11 @@
+import { Button, Row } from "react-bootstrap"
+
+const Main = () => {
+    return (
+        <Row>
+            <Button>Начать</Button>
+        </Row>
+    )
+}
+
+export default Main;
